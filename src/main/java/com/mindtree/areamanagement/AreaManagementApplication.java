4348395d1d9d667bf599f9d64028b67c9d8c4168
MamataSahoo <mamatasahoo758@gmail.com>
2021-07-28 12:12:17 +0530

    new changes

--- src/main/java/com/mindtree/areamanagement/AreaManagementApplication.java
+++ src/main/java/com/mindtree/areamanagement/AreaManagementApplication.java
@@ -8,6 +8,10 @@ public class AreaManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AreaManagementApplication.class, args);
+		int a=87;
+		int b=87;
+		int s=a+b;
+		System.out.println(s);
 	}
 
 }
